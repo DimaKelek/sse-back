ba6a1d79346f54792453ab200e54629543322d40
Kelek <kelekofficial@gmail.com>
2022-06-19 00:20:00 +0300

    init

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: 'auto',
+  semi: true,
+  importOrder: [
+    '^(.*)$',
+    '^[./]',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
